1ab95b3fd4064df666021b4a619428a8ca47656f
Amit Patidar <amit.patidar6264@gmail.com>
2026-07-21 18:46:38 +0530

    Create variables.tf

---
+++ modules/gke/variables.tf
@@ -0,0 +1,27 @@
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "network" {
+  type = string
+}
+
+variable "subnetwork" {
+  type = string
+}
+
+variable "pods_range_name" {
+  type = string
+}
+
+variable "services_range_name" {
+  type = string
+}
