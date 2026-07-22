9a81e5b34c853179aba977addc07a0a78240437f
Amit Patidar <amit.patidar6264@gmail.com>
2026-07-22 10:38:45 +0530

    Update variables.tf

--- modules/gke/variables.tf
+++ modules/gke/variables.tf
@@ -25,3 +25,31 @@ variable "pods_range_name" {
 variable "services_range_name" {
   type = string
 }
+
+
+
+
+
+variable "node_service_account" {
+  type = string
+}
+
+
+node_config {
+  service_account = var.node_service_account
+
+  machine_type = "e2-standard-2"
+  disk_size_gb = 50
+  disk_type    = "pd-balanced"
+  image_type   = "COS_CONTAINERD"
+
+  shielded_instance_config {
+    enable_secure_boot          = true
+    enable_integrity_monitoring = true
+  }
+
+  oauth_scopes = [
+    "https://www.googleapis.com/auth/logging.write",
+    "https://www.googleapis.com/auth/monitoring.write"
+  ]
+}
